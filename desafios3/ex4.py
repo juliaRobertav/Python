97d228ed8c7c3adb3595c4407060b7bbddda4a5f
ju <114812014+xulixnha@users.noreply.github.com>
2023-02-28 09:28:52 -0300

    Update ex4.py

--- desafios3/ex4.py
+++ desafios3/ex4.py
@@ -1,7 +1,9 @@
-n = int(input('Digite o número:'))
-for c in range (1, n + 1):
-    if n % c == 0:
-    print(f\\33[31m\33[m)
-    else:
-       print(f\\33[31m\33[m)
-    print('{}'.format(c), end ='')
+num = int(input('Numero: '))
+
+if num >= 1:
+    for i in range(1, num ):
+        if num % 1 == 0:
+            print(num, 'é primo')
+        else:
+            print(num, 'não é primo')
+            break
