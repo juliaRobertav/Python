e8f7771ccd8f7d3478a0cb6ebb2df3ba6f29551e
julinha <114812014+xulixnha@users.noreply.github.com>
2023-04-13 05:06:07 -0300

    Update jogo_velha.py

--- Bosch/jogo_velha.py
+++ Bosch/jogo_velha.py
@@ -1,47 +1,51 @@
 ##comandos##
-#while para resposta
-#for para posições
-#while true para jogadas
-#jogadores
-#ganhador
-#exibir tabela
-#MQTT(pesquisar mais)
-#não esquecer de aplicar no main
-#jogador escolher a posição e "X" ou "O"
-
-class Jogo:
-        
-    def __init__(self,criar_tabela, jogadas, jogo, tabela):
-        self.criar_tabela()
-        self.jogadas()
-        self.jogo()
-        self.tabela = tabela
-
-    def criar_tabela(self):
-        """
-        "Função para criar o tabuleiro"
-        :param tabela: matriz
-        """
-        tabela = [[0, 1, 2],
-                 [3, 4, 5],
-                 [6, 7, 8]
-                 ]
+# while para resposta
+# for para posições
+# while true para jogadas
+# jogadores
+# ganhador
+# exibir tabela
+# MQTT(pesquisar mais)
+# não esquecer de aplicar no main
+# jogador escolher a posição e "X" ou "O"
 
 
-    def jogadas(self):
-        """
-        "Função onde calcula as jogadas"
-        :param jogadas
-        """
-        jogada == 0
-        while True:
-            for i in range(3):
-                for j in range(3):
-                    if tabela[i][j] == 0:
-                        print(" _ ", end=' ')
-                    elif tabela[i][j] == 1:
-                        print(" ❌ ", end=' ')
-                    elif tabela[i][j] == 1:
-                        print(" ⭕ ", end=' ')
+def tabuleiro():
+    tab = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]
+           ]
 
-            print()
+    print("   |   |   ")
+    print("---|---|---")
+    print("   |   |   ")
+    print("---|---|---")
+    print("   |   |   ")
+    print(tab)
+
+
+def jogadas():
+    while True:
+        try:
+            jogada = int(input("Digite a posição em que deseja jogar:"))
+            for jogada in range(1, 10):
+                return jogada
+        except ValueError:
+            print("Verifique novamente, o número não está no tabuleiro ou isto não é um número...")
+            continue
+
+
+def posicao(posicao_jogador):
+    pos = jogadas() - 1
+    tabuleiro[pos] = posicao_jogador
+    if tabuleiro[pos] == "X" or tabuleiro[pos] == "O":
+        print("\nEspaço já ocupado. Tente colocar em outro.")
+    else:
+
+
+
+if __name__ == "__main__":
+    tabuleiro()
+    jogadas()
+    posicao()
+
